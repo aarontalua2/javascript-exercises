ada1388b801dec71528b9ff9a9ad3604da739250
aarontalua2 <aarontalua14@gmail.com>
2023-03-21 21:26:09 +0800

    exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  sum = 0;
 
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    typeof num1 != `number` ||
+    typeof num2 != `number`
+  ) {
+    return "ERROR";
+  } else if (num1 < num2) {
+    while (num1 <= num2) {
+      sum += num1;
+      num1++;
+    }
+    return sum;
+  } else if (num2 < num1) {
+    while (num2 <= num1) {
+      sum += num2;
+      num2++;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
