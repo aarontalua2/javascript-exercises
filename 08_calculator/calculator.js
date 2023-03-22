c60e0fc5d059b291206eb4dda8f6ecfd0e117ea3
aarontalua2 <aarontalua14@gmail.com>
2023-03-22 15:46:02 +0800

    Answered calculator exercise -- not required yet

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function (...args) {
+  let sum = 0;
+  for (let arg of args) {
+    sum += arg;
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, ...args) {
+  let difference = num1;
+  for (let arg of args) {
+    difference -= arg;
+  }
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  for (item of array) {
+    sum += item;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let product = 1;
+  for (item of array) {
+    product *= item;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  let ans = base;
+  for (let counter = 1; counter != exponent; counter++) {
+    ans *= base;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num == 0 || num == 1) return 1;
+
+  for (counter = num - 1; counter >= 1; counter--) {
+    num *= counter;
+  }
+  return num;
 };
 
 // Do not edit below this line
@@ -29,5 +54,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
